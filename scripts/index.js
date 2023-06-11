12d1acc7b22eb95ca6cc7fcdcfbe019cc0451a9e
MrRyushi <john_patrick_marcellana@dlsu.edu.ph>
2023-06-12 02:22:37 +0800

    added a confirm prompt when clicking a seat button, adjusted some margins

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,16 @@ function backToIndex(){
     window.location.href = "computer_labs.html";
 }
 
+const seatBtn = document.querySelectorAll(".seatBtn");
+for(let i = 0; i < seatBtn.length + 1; i++){
+    seatBtn[i].addEventListener("click", confirmReserve);
+}
+
+function confirmReserve(){
+    var date = document.querySelector("#date").value;
+    var time = document.querySelector("#time").value;
+    var reserve = confirm(`Reseve this seat on ${date} at ${time}?`);
+}
+
 document.getElementById('date').valueAsDate = new Date();
+
