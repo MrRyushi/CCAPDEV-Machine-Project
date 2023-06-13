5436f6a2295dbc9dbb33623a550d3d820e881594
MrRyushi <john_patrick_marcellana@dlsu.edu.ph>
2023-06-14 02:13:23 +0800

    added a todo list, modified for seatbtn to turn red when someone reserves, added the my reservations page

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,30 @@ function backToIndex(){
     window.location.href = "computer_labs.html";
 }
 
+var reserve;
+
 const seatBtn = document.querySelectorAll(".seatBtn");
-for(let i = 0; i < seatBtn.length + 1; i++){
-    seatBtn[i].addEventListener("click", confirmReserve);
+for (let i = 0; i < seatBtn.length; i++) {
+  seatBtn[i].addEventListener("click", confirmReserve.bind(null, seatBtn[i]));
 }
 
-function confirmReserve(){
-    var date = document.querySelector("#date").value;
-    var time = document.querySelector("#time").value;
-    var reserve = confirm(`Reseve this seat on ${date} at ${time}?`);
+function confirmReserve(button) {
+  var date = document.querySelector("#date").value;
+  var time = document.querySelector("#time").value;
+
+  if (date !== "" && time !== "") {
+    reserve = confirm(`Reserve this seat on ${date} at ${time}?`);
+  } else {
+    alert('Please enter a date and time!');
+  }
+
+  if (reserve === true) {
+    // Change the color of the button
+    button.classList.replace("btn-info", "btn-danger");
+  }
 }
+
 
 document.getElementById('date').valueAsDate = new Date();
+
 
